4ba707304c0565896a4aa5a676cde2e37e72b97b
gjburke2 <gjburke2@illinois.edu>
2026-04-24 15:52:07 -0500

    Deleted onLogout, no need to log out for now

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="flex-1 flex w-full h-full">
       {isAuthenticated ? (
-        <Scratchpad onLogout={() => setIsAuthenticated(false)} />
+        <Scratchpad />
       ) : (
         <AuthScreen onLogin={() => setIsAuthenticated(true)} />
       )}
